1579ea0a382e6a1010037ccd270c1629c14fb9cf
skrtttgbt <147715266+skrtttgbt@users.noreply.github.com>
2026-04-16 18:15:30 +0800

    Api communication and Postman

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/products', function () {
-    return Product::all();
-});
+// Route::get('/products', function () {
+//     return Product::all();
+// });
